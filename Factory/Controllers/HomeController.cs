1ca06aa6aff883300cc9c66dfd4d468a843abf89
Christinamawel <christinamawel@gmail.com>
2022-01-07 15:53:41 -0800

    add ability to see two models in Home/Index View

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      List<Machine> machineModel = _db.Machines.ToList();
+      List<Engineer> engineerModel = _db.Engineers.ToList();
+      ViewBag.Engineers = engineerModel;
+      ViewBag.Machines = machineModel;
       return View();
     }
   }
